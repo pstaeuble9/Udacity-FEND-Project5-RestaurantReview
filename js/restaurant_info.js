7ccf3d9f0814c33e0b85a6ecf24b94c6028102fb
Patrick Staeuble <35965091+pstaeuble9@users.noreply.github.com>
2019-03-18 16:03:03 -0400

    Add files via upload

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: '<pk.eyJ1Ijoic3RhZXViczc5IiwiYSI6ImNqdGVudGx2aTFqdnQzeW5yOTljOTF6dW0ifQ.qPiknW9kv-Vyh3_qAtqcqA>',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
